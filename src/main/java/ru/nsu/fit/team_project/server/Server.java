cbdb4b4d8a5de60b1bab5afe276a4ca0d1d28b84
pkrasnyanskii <p.krasnyanskii@g.nsu.ru>
2022-11-22 18:59:40 +0700

    Local server with echo

---
+++ src/main/java/ru/nsu/fit/team_project/server/Server.java
@@ -0,0 +1,38 @@
+package ru.nsu.fit.team_project.server;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Server {
+    public static void main(String[] args) {
+        connectToServer();
+    }
+
+    public static void connectToServer() {
+        try(ServerSocket serverSocket = new ServerSocket(9991)) {
+            Socket connectionSocket = serverSocket.accept();
+
+            InputStream inputToServer = connectionSocket.getInputStream();
+            OutputStream outputFromServer = connectionSocket.getOutputStream();
+
+            Scanner scanner = new Scanner(inputToServer, "UTF-8");
+            PrintWriter serverPrintOut = new PrintWriter(new OutputStreamWriter(outputFromServer, "UTF-8"), true);
+
+            serverPrintOut.println("Hello World! Enter Peace to exit.");
+
+            boolean done = false;
+            while(!done && scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                serverPrintOut.println("Echo from <Your Name Here> Server: " + line);
+
+                if(line.toLowerCase().trim().equals("peace")) {
+                    done = true;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
