54ec88d74c7ef055514699d4aefedf53be11fb8c
Ivan <Streafyhs@gmail.com>
2022-12-07 10:54:28 +0700

    implemented basic UI

---
+++ src/main/java/ru/nsu/fit/team_project/plannerscape/Task.java
@@ -0,0 +1,47 @@
+package ru.nsu.fit.team_project.plannerscape;
+
+public class Task {
+    private String name;
+    private String description;
+    private TaskStatus status;
+
+    public Task(String name, String description) {
+        this.name = name;
+        this.description = description;
+        status = TaskStatus.ACTIVE;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public TaskStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(TaskStatus status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", status=" + status +
+                '}';
+    }
+}
+
