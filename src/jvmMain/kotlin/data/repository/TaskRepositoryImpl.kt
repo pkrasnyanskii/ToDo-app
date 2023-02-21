0fed31b9bd36b2329595668d6226e328486c4c04
Ivan <Streafyhs@gmail.com>
2023-02-21 09:55:37 +0700

    changed task repository

--- src/jvmMain/kotlin/data/repository/TaskRepositoryImpl.kt
+++ src/jvmMain/kotlin/data/repository/TaskRepositoryImpl.kt
@@ -3,6 +3,7 @@ package data.repository
 import data.converter.TaskConverter
 import data.model.Model
 import domain.entity.Task
+import domain.entity.TaskStatus
 import domain.repository.TaskRepository
 
 class TaskRepositoryImpl(
@@ -13,6 +14,16 @@ class TaskRepositoryImpl(
     override fun getTasks(): List<Task> =
         model.objects
             .map { obj -> converter.convert(obj) }
+
+    override fun getActiveTasks(): List<Task> =
+        model.objects
+            .map { obj -> converter.convert(obj) }
+            .filter { task -> task.status == TaskStatus.ACTIVE }
+
+    override fun getCompletedTasks(): List<Task> =
+        model.objects
+            .map { obj -> converter.convert(obj) }
+            .filter { task -> task.status == TaskStatus.COMPLETED }
 
     override fun addTask(task: Task) {
         TODO("Not yet implemented")
