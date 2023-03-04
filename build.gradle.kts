f95e7f0600f38c7b0f646701e1d332cb01c1c153
Ivan <Streafyhs@gmail.com>
2023-03-04 17:35:57 +0700

    added retrofit library to work with network

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,6 +29,10 @@ kotlin {
                 implementation(compose.desktop.currentOs)
                 implementation("com.google.code.gson:gson:2.10.1")
                 implementation("io.insert-koin:koin-core:3.3.3")
+
+                implementation("com.squareup.retrofit2:retrofit:2.9.0")
+                implementation("com.squareup.retrofit2:retrofit-mock:2.9.0")
+                implementation("com.squareup.retrofit2:converter-gson:2.9.0")
             }
         }
         val jvmTest by getting
