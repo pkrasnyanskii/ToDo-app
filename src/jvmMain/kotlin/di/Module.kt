42e944e037df0cc0d859bfc489fa13e5fdb00839
Ivan <Streafyhs@gmail.com>
2023-03-04 17:53:00 +0700

    configured di for network requests

--- src/jvmMain/kotlin/di/Module.kt
+++ src/jvmMain/kotlin/di/Module.kt
@@ -1,29 +1,39 @@
 package di
 
+import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import data.commands.CommandExecutor
 import data.commands.CommandsStorage
 import data.commands.entity.Command
+import data.converter.CommandConverter
 import data.converter.TaskConverter
 import data.model.Model
 import data.model.ObjectsStorage
+import data.network.CommandsApi
+import data.repository.CommandRepositoryImpl
 import data.repository.TaskRepositoryImpl
 import data.serialization.CommandDeserializer
 import data.serialization.Deserializer
 import data.serialization.Serializer
+import domain.repository.CommandRepository
 import domain.repository.TaskRepository
-import domain.usecase.AddTaskUseCase
-import domain.usecase.ChangeTaskStatusUseCase
-import domain.usecase.DeleteTaskUseCase
-import domain.usecase.GetTasksUseCase
+import domain.usecase.*
+import okhttp3.OkHttpClient
 import org.koin.dsl.module
 import presentation.TasksStateHolder
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+private const val COMMANDS_BASE_URL = "http://127.0.0.1:8080"
+private const val CONNECTION_TIMEOUT = 5L
+private const val WRITE_TIMEOUT = 5L
+private const val READ_TIMEOUT = 5L
+
 
 val data = module {
-    single { GsonBuilder()
-        .registerTypeAdapter(Command::class.java, CommandDeserializer())
-        .setPrettyPrinting()
-        .create()
+    single {
+        provideGson()
     }
     single { Deserializer(get()) }
     single { CommandsStorage(get()) }
@@ -32,7 +42,41 @@ val data = module {
     single { CommandExecutor(get()) }
     single { Model(get(), get(), get()) }
     single { TaskConverter() }
+    single { CommandConverter() }
+    single {
+        provideOkHttpClient()
+    }
+    single {
+        provideRetrofit(get())
+    }
+    single {
+        provideCommandsApi(get())
+    }
 }
+
+fun provideOkHttpClient(): OkHttpClient =
+    OkHttpClient().newBuilder()
+        .connectTimeout(CONNECTION_TIMEOUT, TimeUnit.SECONDS)
+        .writeTimeout(WRITE_TIMEOUT, TimeUnit.SECONDS)
+        .readTimeout(READ_TIMEOUT, TimeUnit.SECONDS)
+        .build()
+
+fun provideGson(): Gson =
+    GsonBuilder()
+        .registerTypeAdapter(Command::class.java, CommandDeserializer())
+        .setPrettyPrinting()
+        .create()
+
+fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit =
+    Retrofit.Builder()
+        .client(okHttpClient)
+        .baseUrl(COMMANDS_BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create(provideGson()))
+        .build()
+
+fun provideCommandsApi(retrofit: Retrofit): CommandsApi =
+    retrofit.create(CommandsApi::class.java)
+
 
 val domain = module {
     single<TaskRepository> { TaskRepositoryImpl(get(), get(), get()) }
@@ -40,6 +84,8 @@ val domain = module {
     single { GetTasksUseCase(get()) }
     single { ChangeTaskStatusUseCase(get()) }
     single { DeleteTaskUseCase(get()) }
+    single<CommandRepository> { CommandRepositoryImpl(get(), get(), get(), get()) }
+    single { GetDataUseCase(get()) }
 }
 
 val presentation = module {
