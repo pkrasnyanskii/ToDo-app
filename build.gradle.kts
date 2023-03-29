b645e8914cd537b9c3b624e9f2e7bb9b185c79e6
Ivan <Streafyhs@gmail.com>
2023-03-30 02:56:52 +0700

    changed build.gradle.kts to build windows native distributable

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,9 +43,18 @@ compose.desktop {
     application {
         mainClass = "MainKt"
         nativeDistributions {
+            modules("java.instrument", "java.sql", "jdk.unsupported")
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
             packageName = "PlannerScape"
             packageVersion = "1.0.0"
+            windows {
+                // a version for all Windows distributables
+                packageVersion = "1.0.0"
+                // a version only for the msi package
+                msiPackageVersion = "1.0.0"
+                // a version only for the exe package
+                exePackageVersion = "1.0.0"
+            }
         }
     }
 }
