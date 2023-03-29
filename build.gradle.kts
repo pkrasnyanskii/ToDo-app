11f2f1cf05adaaeb6012d9d52d27ddbd79841def
Ivan <Streafyhs@gmail.com>
2023-03-30 02:57:28 +0700

    updated koin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,8 +28,7 @@ kotlin {
             dependencies {
                 implementation(compose.desktop.currentOs)
                 implementation("com.google.code.gson:gson:2.10.1")
-                implementation("io.insert-koin:koin-core:3.3.3")
-
+                implementation("io.insert-koin:koin-core:3.4.0")
                 implementation("com.squareup.retrofit2:retrofit:2.9.0")
                 implementation("com.squareup.retrofit2:retrofit-mock:2.9.0")
                 implementation("com.squareup.retrofit2:converter-gson:2.9.0")
